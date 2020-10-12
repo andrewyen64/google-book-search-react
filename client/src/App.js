9be87e6756b4c3b09e115503cf88d45ef5f41884
Andrew <andrewyen64@gmail.com>
2020-10-12 15:40:09 -0700

    Setup react routing in App.js and add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar/Navbar.js";
+// import Search from "./pages/Search";
+// import Saved from "./pages/Saved";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+function App() {
+  return (
+      <Router>
+        <div>
+        <NavBar />
+        {/* <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch> */}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+      </Router>
+  );
+};
 
 export default App;
