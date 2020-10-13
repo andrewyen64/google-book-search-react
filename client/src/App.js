35727e148faf02efd12d0d0af33150c724e9c416
Andrew <andrewyen64@gmail.com>
2020-10-12 18:03:35 -0700

    Add Saved page component and enabled on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/Navbar.js";
 import Search from "./pages/Search";
-// import Saved from "./pages/Saved";
+import Saved from "./pages/Saved";
 import "./App.css";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/search" component={Search} />
-          {/* <Route exact path="/saved" component={Saved} /> */}
+          <Route exact path="/saved" component={Saved} />
         </Switch>
         </div>
       </Router>
